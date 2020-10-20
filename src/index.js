8f6fd0bd8db58447f88a5a0e8961692011fdd978
Kota <jatak307@gmail.com>
2020-10-20 16:05:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (!str % 2) return;
+
+  let result = [];
+  let bracketsObj = Object.fromEntries(bracketsConfig);
+
+  for (elem of str.split('')) {
+    if (result[0] === elem) {
+      result.shift();
+    } else if (bracketsObj[elem]) {
+      result.unshift(bracketsObj[elem]);
+    } else {
+      return false;
+    }
+  }
+
+  return result.length === 0;
 }
